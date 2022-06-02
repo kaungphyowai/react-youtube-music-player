20e483bb7d48727f841dcd65ea5194be37fd2aa7
yaungnilinn <104423073+yaungnilinn@users.noreply.github.com>
2022-06-02 15:36:04 +0630

    initial

---
+++ src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  const [formData, setFormData] = React.useState({
+    firstName:"",
+    lastName:"",
+    
+  })
+
+  function handleChange(event){
+    const {name, value} = event.target
+    setFormData(prevFormData=>{
+      return{
+        ...prevFormData,
+        [name] : value
+      }
+    })
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    //get data(name) from 
+    console.log(formData);
+    //get data(name) from
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label for="firstName">First Name</label><br />
+      <input
+      type="text"
+      id="firstName"
+      name="firstName"
+      value={formData.firstName}
+      onchange={handleChange}
+      
+      />
+      <br />
+
+      <label for="lastName">Last Name</label><br />
+      <input
+      type="text"
+      id="lastName" 
+      name="lastName"
+      value={formData.lastName}
+      onchange={handleChange}
+      />
+      <br />
+      <br />
+      {/*Google Buttton*/}
+      <button>Login with Google</button>
+      {/*Google Buttton*/}
+    </form>
+  )
+}
+
+export default App;
