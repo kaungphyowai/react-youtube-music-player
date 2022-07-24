172d7f5cbb622c8da764153a127dfce710da1735
kaungphyowai <kaungphyowaidecember@gmail.com>
2022-07-24 15:25:24 +0630

    fixing not log in bug

--- src/Pages/LoginGoogle.js
+++ src/Pages/LoginGoogle.js
@@ -1,20 +1,12 @@
 import "../App.css";
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import signInWithGoogle from "../firebase/auth";
-import { getAuth } from "firebase/auth";
 
 function LoginGoogle() {
-  let navigate = useNavigate();
 
   async function handleClick(e) {
     e.preventDefault();
-    // const user = await signInWithGoogle();
-    console.log(getAuth().currentUser)
-    // if (user) {
-
-    //   navigate("/music-input");
-    // }
+    const user = await signInWithGoogle();
   }
 
   return (
